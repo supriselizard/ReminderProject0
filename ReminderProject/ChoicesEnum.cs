18a38b260d8c5fc46b065f342bd0753103313fc2
surpriselizard <52936787+surpriselizard@users.noreply.github.com>
2019-07-16 15:50:35 -0600

    Final Update until First Release

---
+++ ReminderProject/ChoicesEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReminderProject
+{
+    public enum ChoicesEnum
+    {
+        AddAReminder = 1,
+        DisplaySingleRem,
+        DisplayWeekAhead,
+        DisplayMonth,
+        Exit
+    }
+}
